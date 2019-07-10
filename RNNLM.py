dc35bc3ab23b886cc663011d39c2eaedc4383bd9
Brian Crafton <crafton.b@husky.neu.edu>
2019-07-10 15:31:59 -0400

    print statements we made trying to disect the implementation

--- RNNLM.py
+++ RNNLM.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+np.set_printoptions(threshold=np.inf)
 import math
 
 class RNNLM(object):
@@ -31,10 +32,8 @@ class RNNLM(object):
 
         # We set a dynamic learining rate, it decays every time the model has gone through 150 batches.
         # A minimum learning rate has also been set.
-        self.learning_rate = tf.train.exponential_decay(initial_learning_rate, self.global_step,
-                                           150, 0.96, staircase=True)
-        self.learning_rate = tf.cond(tf.less(self.learning_rate, final_learning_rate), lambda: tf.constant(final_learning_rate),
-                                     lambda: self.learning_rate)
+        self.learning_rate = tf.train.exponential_decay(initial_learning_rate, self.global_step, 150, 0.96, staircase=True)
+        self.learning_rate = tf.cond(tf.less(self.learning_rate, final_learning_rate), lambda: tf.constant(final_learning_rate), lambda: self.learning_rate)
 
         self.dropout_rate = tf.placeholder(tf.float32, name="dropout_rate")
 
@@ -46,14 +45,19 @@ class RNNLM(object):
             line_split = tf.string_split([line])
             input_seq = tf.string_to_number(line_split.values[:-1], out_type=tf.int32)
             output_seq = tf.string_to_number(line_split.values[1:], out_type=tf.int32)
+
+            # line = tf.Print(line, [self.batch_size, line], message='', summarize=1000)
+            
+            # line = tf.Print(line, [input_seq], message='', summarize=1000)
+            # line = tf.Print(line, [output_seq], message='', summarize=1000)
+
             return input_seq, output_seq
 
         training_dataset = tf.data.TextLineDataset(self.file_name_train).map(parse).shuffle(256).padded_batch(self.batch_size, padded_shapes=([None], [None]))
         validation_dataset = tf.data.TextLineDataset(self.file_name_validation).map(parse).padded_batch(self.batch_size, padded_shapes=([None], [None]))
         test_dataset = tf.data.TextLineDataset(self.file_name_test).map(parse).batch(1)
 
-        iterator = tf.contrib.data.Iterator.from_structure(training_dataset.output_types,
-                                              training_dataset.output_shapes)
+        iterator = tf.data.Iterator.from_structure(training_dataset.output_types, training_dataset.output_shapes)
 
         self.input_batch, self.output_batch = iterator.get_next()
 
@@ -61,29 +65,19 @@ class RNNLM(object):
         self.validation_init_op = iterator.make_initializer(validation_dataset)
         self.test_init_op = iterator.make_initializer(test_dataset)
 
-
         # Input embedding mat
-        self.input_embedding_mat = tf.get_variable("input_embedding_mat",
-                                                   [self.vocab_size, self.num_hidden_units],
-                                                   dtype=tf.float32)
-
+        self.input_embedding_mat = tf.get_variable("input_embedding_mat", [self.vocab_size, self.num_hidden_units], dtype=tf.float32)
         self.input_embedded = tf.nn.embedding_lookup(self.input_embedding_mat, self.input_batch)
 
         # LSTM cell
         cell = tf.contrib.rnn.LSTMCell(self.num_hidden_units, state_is_tuple=True)
         cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=self.dropout_rate)
         cell = tf.contrib.rnn.MultiRNNCell(cells=[cell]*self.num_layers, state_is_tuple=True)
-
         self.cell = cell
 
         # Output embedding
-        self.output_embedding_mat = tf.get_variable("output_embedding_mat",
-                                                    [self.vocab_size, self.num_hidden_units],
-                                                    dtype=tf.float32)
-
-        self.output_embedding_bias = tf.get_variable("output_embedding_bias",
-                                                     [self.vocab_size],
-                                                     dtype=tf.float32)
+        self.output_embedding_mat = tf.get_variable("output_embedding_mat", [self.vocab_size, self.num_hidden_units], dtype=tf.float32)
+        self.output_embedding_bias = tf.get_variable("output_embedding_bias", [self.vocab_size], dtype=tf.float32)
 
         non_zero_weights = tf.sign(self.input_batch)
         self.valid_words = tf.reduce_sum(non_zero_weights)
@@ -95,30 +89,27 @@ class RNNLM(object):
             return real_length
 
         batch_length = get_length(non_zero_weights)
-
+        # batch_length = tf.Print(batch_length, [batch_length], message='', summarize=1000)
 
         # The shape of outputs is [batch_size, max_length, num_hidden_units]
-        outputs, _ = tf.nn.dynamic_rnn(
-            cell=self.cell,
-            inputs=self.input_embedded,
-            sequence_length=batch_length,
-            dtype=tf.float32
-        )
+        outputs, _ = tf.nn.dynamic_rnn(cell=self.cell, inputs=self.input_embedded, sequence_length=batch_length, dtype=tf.float32)
 
         def output_embedding(current_output):
-            return tf.add(
-                tf.matmul(current_output, tf.transpose(self.output_embedding_mat)), self.output_embedding_bias)
+            return tf.add(tf.matmul(current_output, tf.transpose(self.output_embedding_mat)), self.output_embedding_bias)
 
         # To compute the logits
+        labels = tf.reshape(self.output_batch, [-1])
+
         logits = tf.map_fn(output_embedding, outputs)
         logits = tf.reshape(logits, [-1, vocab_size])
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.reshape(self.output_batch, [-1]), logits=logits) \
-               * tf.cast(tf.reshape(non_zero_weights, [-1]), tf.float32)
 
+        logits = tf.Print(logits, [tf.shape(logits), tf.shape(labels)], message='', summarize=1000)
+
+        # because of how many 0==0 we will have, this makes me question our perplexity score here.
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits) * tf.cast(tf.reshape(non_zero_weights, [-1]), tf.float32)
         self.loss = loss
 
         # Train
-
         params = tf.trainable_variables()
 
         opt = tf.train.AdagradOptimizer(self.learning_rate)
@@ -141,9 +132,29 @@ class RNNLM(object):
             while True:
 
                 try:
-                    _loss, _valid_words, global_step, current_learning_rate, _ = sess.run(
-                        [self.loss, self.valid_words, self.global_step, self.learning_rate, self.updates],
-                        {self.dropout_rate: 0.5})
+                    _loss, _valid_words, _, i, o = sess.run([self.loss, self.valid_words, self.updates, self.input_batch, self.output_batch], {self.dropout_rate: 0.5})
+                    
+                    # print (len(i))
+                    # print (len(o))                    
+
+                    for sentence in i:
+                        print (np.count_nonzero(sentence == 0), np.count_nonzero(sentence), len(sentence))
+
+                    count = 0
+                    for sentence in i:
+                        count += len(sentence)
+                    # print (count)
+
+                    count = 0
+                    for sentence in o:
+                        count += len(sentence)
+                    # print (count)
+
+                    print (i[0])
+                    print (o[0])
+
+                    assert (False)
+
                     train_loss += np.sum(_loss)
                     train_valid_words += _valid_words
 
@@ -151,8 +162,8 @@ class RNNLM(object):
 
                         train_loss /= train_valid_words
                         train_ppl = math.exp(train_loss)
-                        print "Training Step: {}, LR: {}".format(global_step, current_learning_rate)
-                        print "    Training PPL: {}".format(train_ppl)
+                        print ("Training Step: {}, LR: {}".format(global_step, current_learning_rate))
+                        print ("    Training PPL: {}".format(train_ppl))
 
                         train_loss = 0.0
                         train_valid_words = 0
@@ -178,7 +189,7 @@ class RNNLM(object):
                 except tf.errors.OutOfRangeError:
                     dev_loss /= dev_valid_words
                     dev_ppl = math.exp(dev_loss)
-                    print "Validation PPL: {}".format(dev_ppl)
+                    print ("Validation PPL: {}".format(dev_ppl))
                     if dev_ppl < best_score:
                         patience = 5
                         saver.save(sess, "model/best_model.ckpt")
@@ -208,6 +219,8 @@ class RNNLM(object):
                 _dev_loss, _dev_valid_words, input_line = sess.run(
                     [self.loss, self.valid_words, self.input_batch],
                     {self.dropout_rate: 1.0})
+ 
+                assert(False)
 
                 dev_loss = np.sum(_dev_loss)
                 dev_valid_words = _dev_valid_words
@@ -218,9 +231,9 @@ class RNNLM(object):
                 if verbose:
                     dev_loss /= dev_valid_words
                     dev_ppl = math.exp(dev_loss)
-                    print raw_line + "    Test PPL: {}".format(dev_ppl)
+                    print (raw_line + "    Test PPL: {}".format(dev_ppl))
 
             global_dev_loss /= global_dev_valid_words
             global_dev_ppl = math.exp(global_dev_loss)
-            print "Global Test PPL: {}".format(global_dev_ppl)
+            print ("Global Test PPL: {}".format(global_dev_ppl))
 
